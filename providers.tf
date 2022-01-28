afdf4cc7a71b11df5327f6b50fbd353468aa34a0
Mateus Caruccio <mateus.caruccio@getupcloud.com>
2022-01-28 18:13:58 -0300

    cleanup tests

--- providers.tf
+++ providers.tf
@@ -1,22 +1,13 @@
 terraform {
   required_providers {
-    cronitor = {
-      source = "nauxliu/cronitor"
-      #     version = "1.0.8"
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = "~> 1"
     }
 
     shell = {
-      source = "scottwinkler/shell"
-      #      version = "1.7.2"
-    }
-
-    kubectl = {
-      source = "gavinbunney/kubectl"
-      #      version = "~> 1"
-    }
-
-    random = {
-      #      version = "~> 2"
+      source  = "scottwinkler/shell"
+      version = "~> 1"
     }
   }
 }
